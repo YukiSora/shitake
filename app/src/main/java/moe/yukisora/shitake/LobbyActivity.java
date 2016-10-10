fbc5b37001eb8232c0c3db61fdb7f5fcbeb48979
Delacrix Morgan <Delacrix@Tecks-MacBook-Pro.local>
2016-10-10 14:35:55 +0800

    Rounded Green Button - Added to Game Fragments

--- app/src/main/java/moe/yukisora/shitake/LobbyActivity.java
+++ app/src/main/java/moe/yukisora/shitake/LobbyActivity.java
@@ -3,6 +3,8 @@ package moe.yukisora.shitake;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import moe.yukisora.shitake.ui.lobby.HostFragment;
+
 /**
  * Created by Delacrix on 10/10/2016.
  */
@@ -13,9 +15,9 @@ public class LobbyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_generic);
 
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .replace(R.id.activity_main_vg_fragment, new RegisterFragment())
-//                .commit();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.activity_main_vg_fragment, new HostFragment())
+                .commit();
     }
 }
