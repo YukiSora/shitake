4354faf70a5f19af485b97aef6b84531010583cf
Delacrix Morgan <Delacrix@Tecks-MacBook-Pro.local>
2016-10-27 23:02:57 +0800

    Singleton for Deck Created, Question Able to Populate Randomly

---
+++ app/src/main/java/moe/yukisora/shitake/api/DeckAPIClient.java
@@ -0,0 +1,98 @@
+package moe.yukisora.shitake.api;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import moe.yukisora.shitake.model.Deck;
+
+/**
+ * Created by Delacrix on 27/10/2016.
+ */
+
+public class DeckAPIClient {
+
+    private static DeckAPIClient sSharedInstance;
+    private static String sCurrentDeck;
+
+    private int mCounter;
+    private ArrayList<Deck> mDeck = new ArrayList<>();
+    private SharedPreferences mSharedPreferences;
+
+    // Constructor
+    private DeckAPIClient(@NonNull Context context, String filename) {
+        mCounter = 0;
+        sCurrentDeck = filename;
+        mSharedPreferences = context.getSharedPreferences("preference-key", Context.MODE_PRIVATE);
+
+        populateQuestions(context);
+
+        Collections.shuffle(mDeck, new Random(System.nanoTime()));
+
+    }
+
+    // Singleton Instances
+    public static synchronized DeckAPIClient getInstance(@NonNull Context context, String filename) {
+        if (sSharedInstance == null && sCurrentDeck != filename) {
+            sSharedInstance = new DeckAPIClient(context, filename);
+        }
+
+        return sSharedInstance;
+    }
+
+    private StringBuilder loadJSONFromAsset(Context context) {
+        StringBuilder buf = new StringBuilder();
+
+        try {
+            InputStream json = context.getAssets().open(sCurrentDeck+".json");
+            BufferedReader in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                buf.append(str);
+            }
+
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return buf;
+    }
+
+    private void populateQuestions(Context context) {
+        try {
+            JSONObject jsonRootObject = new JSONObject(String.valueOf(loadJSONFromAsset(context)));
+            JSONArray jsonArray = jsonRootObject.optJSONArray(sCurrentDeck);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                mDeck.add(new Deck(jsonObject.optString("Question"), jsonObject.optString("Answer")));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public Deck getDeck(){
+        return mDeck.get(mCounter++);
+    }
+}
--- app/src/main/java/moe/yukisora/shitake/ui/game/QuestionFragment.java
+++ app/src/main/java/moe/yukisora/shitake/ui/game/QuestionFragment.java
@@ -8,8 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import moe.yukisora.shitake.R;
+import moe.yukisora.shitake.api.DeckAPIClient;
 
 
 /**
@@ -18,21 +30,25 @@ import moe.yukisora.shitake.R;
 
 public class QuestionFragment extends Fragment {
 
-    private Button submitButton;
+    private TextView mQuestionTitle;
+    private Button mSubmitButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_question, container, false);
 
-        submitButton = (Button) rootView.findViewById(R.id.submit_button);
+        mQuestionTitle = (TextView) rootView.findViewById(R.id.question_title);
+        mSubmitButton = (Button) rootView.findViewById(R.id.submit_button);
 
         return rootView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        mQuestionTitle.setText(DeckAPIClient.getInstance(getContext(), "isthatafact").getDeck().getmQuestion());
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 QuestionFragment.this.showPendingFragment();
