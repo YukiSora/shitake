5053d9aab3c54db662106683de8e7f5fe242758c
yukisora <yukisora2013@gmail.com>
2016-11-04 01:44:49 +0800

    host fragment

--- app/src/main/java/moe/yukisora/shitake/ui/lobby/HostFragment.java
+++ app/src/main/java/moe/yukisora/shitake/ui/lobby/HostFragment.java
@@ -2,59 +2,31 @@ package moe.yukisora.shitake.ui.lobby;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import java.util.ArrayList;
 
 import moe.yukisora.shitake.R;
-import moe.yukisora.shitake.adapter.HostRecyclerAdapter;
-import moe.yukisora.shitake.model.Deck;
 
 /**
  * Created by Delacrix on 10/10/2016.
  */
 
-public class HostFragment extends Fragment implements DeckViewHolder.OnDeckSelectedListener{
-
-    private RecyclerView mRecyclerView;
-    private Button mJoinButton;
-
+public class HostFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_host, container, false);
+        View view = inflater.inflate(R.layout.fragment_host, container, false);
 
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.deck_recycler_view);
-
-        return rootView;
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Layout Manager
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(layoutManager);
-
-        // Host Recycler Adapter
-        HostRecyclerAdapter mHostRecyclerAdapter = new HostRecyclerAdapter();
-        mHostRecyclerAdapter.setDeckList(setDeckList());
-        mHostRecyclerAdapter.setDeckListener(this);
-
-        mRecyclerView.setAdapter(mHostRecyclerAdapter);
-    }
-
-    @Override
-    public void onDeckSelected(Deck deck) {
-        Snackbar.make(getView(), "Good Mythical Morning", Snackbar.LENGTH_SHORT).show();
     }
 
     private ArrayList<String> setDeckList() {
@@ -74,5 +46,4 @@ public class HostFragment extends Fragment implements DeckViewHolder.OnDeckSelec
 
         return mDeckList;
     }
-
 }
