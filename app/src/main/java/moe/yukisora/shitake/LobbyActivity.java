aed6396a24793a1e142f2fa84bf4fed835fe8e15
yukisora <yukisora2013@gmail.com>
2016-11-04 06:06:31 +0800

    remove useless code

--- app/src/main/java/moe/yukisora/shitake/LobbyActivity.java
+++ app/src/main/java/moe/yukisora/shitake/LobbyActivity.java
@@ -1,13 +1,7 @@
 package moe.yukisora.shitake;
 
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import moe.yukisora.shitake.ui.lobby.MainFragment;
 
@@ -16,39 +10,14 @@ import moe.yukisora.shitake.ui.lobby.MainFragment;
  */
 
 public class LobbyActivity extends AppCompatActivity {
-    private BroadcastReceiver receiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_generic);
 
-        //bluetooth discovery receiver
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (BluetoothDevice.ACTION_FOUND.equals(intent.getAction())) {
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    Log.i("poi", device.getName() + "\n" + device.getAddress());
-                }
-            }
-        };
-        registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-
-//        Bluetooth.getInstance().isEnabled(this);
-//        Bluetooth.getInstance().setDiscoverable(this);
-//        Bluetooth.getInstance().startSearch();
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.activity_main_vg_fragment, new MainFragment())
                 .commit();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        unregisterReceiver(receiver);
     }
 }
