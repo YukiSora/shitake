e0b4eb4cfb93ee76832cd689adc196d7e5061603
yukisora <yukisora2013@gmail.com>
2016-11-12 01:50:55 +0800

    vote fragment

--- app/src/main/java/moe/yukisora/shitake/ui/game/VoteFragment.java
+++ app/src/main/java/moe/yukisora/shitake/ui/game/VoteFragment.java
@@ -1,22 +1,27 @@
 package moe.yukisora.shitake.ui.game;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
+import moe.yukisora.shitake.MainActivity;
 import moe.yukisora.shitake.R;
+import moe.yukisora.shitake.api.AnswerAPIClient;
 
 /**
  * Created by Delacrix on 10/10/2016.
  */
 
 public class VoteFragment extends Fragment{
-    private Button nextButton;
+    private LinearLayout voteAnswerLinearLayout;
 
     public static VoteFragment newInstance() {
         Bundle args = new Bundle();
@@ -29,24 +34,43 @@ public class VoteFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_vote, container, false);
+        View view = inflater.inflate(R.layout.fragment_vote, container, false);
 
-        nextButton = (Button) rootView.findViewById(R.id.next_button);
+        voteAnswerLinearLayout = (LinearLayout)view.findViewById(R.id.voteAnswerLinearLayout);
 
-        return rootView;
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        populateAnswers();
+    }
+
+    public void populateAnswers() {
+        for (String address : AnswerAPIClient.getInstance().getAnswers().keySet())
+            if (!address.equals("correct") && !address.equals(MainActivity.getBluetoothAddress()))
+                addPendingViewFromLayoutResource(voteAnswerLinearLayout, address, AnswerAPIClient.getInstance().getAnswers().get(address));
+    }
+
+    public View addPendingViewFromLayoutResource(LinearLayout linearLayout, final String address, String answer) {
+        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.view_answer, linearLayout, false);
+
+        Button answerButton = (Button)view.findViewById(R.id.view_btn_answer);
+        answerButton.setAllCaps(true);
+        answerButton.setText(answer);
+        answerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // VoteFragment.this.showAnswerFragment();
+            public void onClick(View view) {
+                Log.i("poi", address);
             }
         });
 
-        super.onViewCreated(view, savedInstanceState);
+        linearLayout.addView(view);
+
+        return view;
     }
 
     public void showAnswerFragment() {
