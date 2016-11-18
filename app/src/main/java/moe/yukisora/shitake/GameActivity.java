addbc46117fa4b9c08000aa89edd9f9c05244ba4
yukisora <yukisora2013@gmail.com>
2016-11-19 01:37:44 +0800

    quit game confirm box

--- app/src/main/java/moe/yukisora/shitake/GameActivity.java
+++ app/src/main/java/moe/yukisora/shitake/GameActivity.java
@@ -1,5 +1,7 @@
 package moe.yukisora.shitake;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -24,6 +26,25 @@ public class GameActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.activity_main_vg_fragment, QuestionFragment.newInstance())
                 .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.quit_game))
+                .setMessage(getResources().getString(R.string.are_you_sure_quit_this_game))
+                .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        superOnBackPressed();
+                    }
+                })
+                .setPositiveButton("No", null)
+                .show();
+    }
+
+    private void superOnBackPressed() {
+        super.onBackPressed();
     }
 
     @Override
